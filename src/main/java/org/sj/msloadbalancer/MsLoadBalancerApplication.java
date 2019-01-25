4613b8759717b69aa56316a579b0fecf0f6088ef
sujitrs <suryawanshisujeet@gmail.com>
2019-01-25 14:32:30 +0530

    init

---
+++ src/main/java/org/sj/msloadbalancer/MsLoadBalancerApplication.java
@@ -0,0 +1,14 @@
+package org.sj.msloadbalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLoadBalancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLoadBalancerApplication.class, args);
+	}
+
+}
+
